8d6e5920ab82ae946333d3f03a6d74f1c09a6b20
Chris Oriente <chrisoriente96@gmail.com>
2020-06-29 04:12:41 -0400

    Enemy and Player Abilities
    I've created an enemy that follows the player, and on collision a message is sen to the editor. The player can also click on and enemy and freeze it indefinitley. Next steps are creating a timer on the freeze ability, and have enemy damage the player.

---
+++ The Necromancer/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    
+    public Rigidbody2D rb;
+    public float moveSpeed = 3f;
+
+    private GameObject player;
+    private Vector2 moveDir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindMoveDir();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDir * moveSpeed * Time.deltaTime);
+    }
+
+    private void FindMoveDir()
+    {
+        // Find the vector between the enemy and player and normalize it
+        moveDir = player.GetComponent<Rigidbody2D>().position - rb.position;
+        moveDir.Normalize();
+    }
+}
--- The Necromancer/Assets/Scripts/PlayerMovement.cs
+++ The Necromancer/Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 5f; 
     public Rigidbody2D rb;
     public Camera cam;
     Vector2 movement;
-    v
+    Vector2 mousePosition;
 
+    public float clickRadius = 2.5f;
+
+    private EnemyController _enemyMovement;
+    private float waitTime = 2.0f;
+    private float timer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +25,53 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //input
+        //Player Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        // Polling
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Raise Corpse
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(mousePosition, clickRadius);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i].gameObject.tag == "Enemy")
+                {
+                    RaiseCorpse(colliders[i].gameObject);
+                }
+            }
+
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    }
+
+    private void RaiseCorpse(GameObject Enemy)
+    {
+        // At level 1 raise corpse will raise grasping hand to hold the enemy in place for a set amount of time based on the enemy
+        _enemyMovement = Enemy.GetComponent<EnemyController>();
+        float prevSpeed = _enemyMovement.moveSpeed;
+        _enemyMovement.moveSpeed = 0;
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Take Damage");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(mousePosition, clickRadius);
     }
 }
